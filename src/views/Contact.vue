73117da3df9876d9c0bb1dae3875cb266d52bf60
Daniel Melchor <dmh672@gmail.com>
2020-11-14 13:04:55 -0500

    Contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="contact">
     <h1>Contact</h1>
+    <form>
+    	<input type="text" name="name" placeholder="Full name">
+    	<input type="text" name="email" placeholder="Email address">
+    	<textarea name="message" placeholder="Message"></textarea>
+    	<input type="submit" name="submit">
+    </form>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped>	
+
 </style>
 
 <script>
