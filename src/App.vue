c0a46d1614fa259ed26207a37cbaa34d465952da
Daniel Melchor <dmh672@gmail.com>
2020-11-14 17:52:00 -0500

    Home

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
 <div id="app">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">FoodPrint</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link to="/calculator" class="nav-link">Calculator</router-link>
-        <router-link to="/contact" class="nav-link">Contact</router-link>
-        <router-link to="/alternatives" class="nav-link">View alternatives</router-link>
+    <div class="navlinks">
+      <a class="navbar-brand" href="/bostonhacks/">FoodPrint</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link to="/calculator" class="nav-link">Calculator</router-link>
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+          <router-link to="/alternatives" class="nav-link">View alternatives</router-link>
+        </div>
       </div>
     </div>
   </nav>
@@ -18,4 +20,8 @@
 </template>
 <style lang="scss" scoped>
 @import "./assets/css/reset.css";
+
+.navlinks {
+  width: 60vw;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,73 @@
 <template>
-  <div class="home">Test</div>
+  <div class="container">
+  	<div class="info-home-container">
+  		<div class="info-home">
+	  		<h1>FoodPrint</h1>
+	  		<p>Calculating your individual carbon footprint in a click</p>
+	  		<a href="/calculator">Calculate mine!</a>
+  		</div>
+  	</div>
+  	<img src="@/assets/img/slide1.png">
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$main-green : #88e3c5;
+
+.container {
+	position: relative;
+	width: 60vw;
+
+	img {
+		display: block;
+		width: 60vw;
+	}
+
+	.info-home-container {
+		background: rgba(0,0,0,0.4);
+		width: 60vw;
+		height: 100%;
+		position: absolute;
+		top: 0;
+
+		.info-home {
+			top: 50%;
+			left: 10%;
+			transform: translateY(-50%);
+			position: absolute;
+			display: block;
+			color: white;
+
+			h1 {
+				color: white;
+				font-size: 4em;
+				position: relative;
+				margin-bottom: 15px;
+				font-weight: bold;
+			}
+
+			p {
+				color: white;
+				font-size: 1.5em;
+			}
+
+			a {
+				padding: 1vw;
+				background-color: transparent;
+				border: 1px solid white;
+				border-radius: 5px;
+				color: white;
+				font-size: 1.1em;
+				text-decoration: none;	
+				display: block;
+				width: 10vw;
+				text-align: center;
+				margin-top: 15%;
+			}
+		}
+	}
+}
+</style>
 
 <script>
 export default {
