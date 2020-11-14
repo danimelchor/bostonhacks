e05104357465f10a2bc56cb543fb04a598312d76
Daniel Melchor <dmh672@gmail.com>
2020-11-14 15:41:53 -0500

    Navbar fix + AppVue fix

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<div id="app">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/">FoodPrint</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,12 +7,14 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" href="/calculator">Calculator</a>
-        <a class="nav-link" href="/contact">Contact</a>
-        <a class="nav-link" href="/alternatives">View alternatives</a>
+        <router-link to="/calculator" class="nav-link">Calculator</router-link>
+        <router-link to="/contact" class="nav-link">Contact</router-link>
+        <router-link to="/alternatives" class="nav-link">View alternatives</router-link>
       </div>
     </div>
   </nav>
+  <router-view/>
+</div>
 </template>
 <style lang="scss" scoped>
 @import "./assets/css/reset.css";
