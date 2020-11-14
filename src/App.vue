abd525640508446ead4b1ac0d60799ef1dee62f4
Daniel Melchor <dmh672@gmail.com>
2020-11-14 11:33:53 -0500

    AppVue changes + css reset

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/calculator">Calculator</router-link> |
-      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/calculator">Calculator</router-link>
+      <router-link to="/contact">Contact</router-link>
       <router-link to="/alternatives">View alternatives</router-link>
     </div>
     <router-view />
   </div>
 </template>
-
 <style lang="scss">
-#app {
+@import './assets/css/reset.css';
 
+$bg : #555;
+
+#app {
 }
 
 #nav {
+  background-color: $bg;
 
+  a {
+    color: white;
+    text-decoration: none;
+  }
 }
 </style>
