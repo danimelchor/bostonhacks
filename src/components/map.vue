33142462a8bfbebe66b624038da07195915fa366
Daniel Melchor <dmh672@gmail.com>
2020-11-14 19:06:59 -0500

    Map type changed

--- src/components/map.vue
+++ src/components/map.vue
@@ -8,12 +8,12 @@
             :height="'100%'"
             :controller="true"
             :useDevicePixels="false"
-            :viewState="{latitude: 49.254, longitude: -123.13, zoom: 1, maxZoom: 16, pitch: 0, bearing: 0}"
+            :viewState="{latitude: 40.73, longitude: -73.75, zoom: 9.6, maxZoom: 16, pitch: 0, bearing: 0}"
             >
       <Mapbox class="fill-wrapper" 
                 :accessToken="'pk.eyJ1IjoiZG1lbGNob3IiLCJhIjoiY2toaWJsNnhyMGEzbTJ6c2RmZWRzY3EyMiJ9.55wKLQ9Yze26jToMLOMQ2g'"                 
-                :center="[-123.13, 49.254]"
-                :zoom="1"
+                :center="[0,30]"
+                :zoom="9.6"
                 :bearing="0"
                 :pitch="0"/>
     </DeckGL>
@@ -22,7 +22,18 @@
 
 <script>
 import {DeckGL, Mapbox} from '@hirelofty/vue_deckgl'
-import {GeoJsonLayer} from '@deck.gl/layers';
+import {ScreenGridLayer} from '@deck.gl/aggregation-layers';
+import {isWebGL2} from '@luma.gl/core';
+
+
+const colorRange = [
+  [255, 255, 178, 25],
+  [254, 217, 118, 85],
+  [254, 178, 76, 127],
+  [253, 141, 60, 170],
+  [240, 59, 32, 212],
+  [189, 0, 38, 255]
+];
 
 export default {
   name: 'Map',
@@ -36,18 +47,16 @@ export default {
       }
   },
   mounted(){
-        this.layers.push(new GeoJsonLayer({
+        this.layers.push(new ScreenGridLayer({
             id: 'mylayer',
-            data: 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json',
+            data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json',
             opacity: 0.8,
-            stroked: false,
-            filled: true,
-            extruded: true,
-            wireframe: true,
-            fp64: true,
-            getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
-            getLineColor: [255, 255, 255],
-            pickable: true,
+            getPosition: d => [d[0], d[1]],
+            getWeight: d => d[2],
+            cellSizePixels: 20,
+            colorRange,
+            gpuAggregation: true,
+            aggregation: 'SUM'
         }))
   }
 }
